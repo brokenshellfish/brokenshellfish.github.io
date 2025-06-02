4718f1c817d23268d81f47a1bfa4b248cb05a123
brokenshellfish <74768801+brokenshellfish@users.noreply.github.com>
2025-06-02 15:09:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,17 +13,19 @@ const htmlEl = document.documentElement;
 // Load saved theme preference
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'light') {
-  htmlEl.classList.add('light-mode');
+  htmlEl.classList.remove('dark-mode');
   toggle.checked = true;
+} else {
+  htmlEl.classList.add('dark-mode');
 }
 
 // Update theme on toggle
 toggle.addEventListener('change', () => {
   if (toggle.checked) {
-    htmlEl.classList.add('light-mode');
+    htmlEl.classList.remove('dark-mode');
     localStorage.setItem('theme', 'light');
   } else {
-    htmlEl.classList.remove('light-mode');
+    htmlEl.classList.add('dark-mode');
     localStorage.setItem('theme', 'dark');
   }
 });
